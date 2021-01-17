bdcef826d97b270c2d8535d8ea88e90b885cba02
shneur <770abhtur1@gmail.com>
2021-01-17 22:22:58 +0200

    dfghj,

--- house/server.js
+++ house/server.js
@@ -11,63 +11,117 @@ app.use(bodyParser.json());
 const mongoose = require('mongoose'); //npm i mongoose
 const url = 'mongodb+srv://KatyaRu:qHO9SxoCGZc6lv7C@cluster0.mfqlq.mongodb.net/test'
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }); //connectin to the db
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}); //connectin to the db
 
 const user = mongoose.model('user', { //collection
-    name: String, //with big letter !!!
-    password: Number,
-    role: String,
-    assignRoom: String
+  name: String, //with big letter !!!
+  password: Number,
+  role: String,
+  assignRoom: String
 });
 
-const Shneor = new user({ name: "Shneor", password: "123", role: "admin", assignRoom: "assignRoom1" })
-    // Shneor.save().then(doc => console.log(doc)).catch(e => {
-    //     console.log(e)
-    // })
 
-const Dudi = new user({ name: "Dudi", password: "456", role: "child", assignRoom: "assignRoom1" })
-const Lior = new user({ name: "Lior", password: "789", role: "child", assignRoom: "assignRoom1" })
-const Katya = new user({ name: "Katya", password: "159", role: "guest", assignRoom: "assignRoom1" })
+const Shneor = new user({
+  name: "Shneor",
+  password: "123",
+  role: "admin",
+  assignRoom: "assignRoom1"
+})
+// Shneor.save().then(doc => console.log(doc)).catch(e => {
+//     console.log(e)
+// })
+
+const Dudi = new user({
+  name: "Dudi",
+  password: "456",
+  role: "child",
+  assignRoom: "assignRoom1"
+})
+const Lior = new user({
+  name: "Lior",
+  password: "789",
+  role: "child",
+  assignRoom: "assignRoom1"
+})
+const Katya = new user({
+  name: "Katya",
+  password: "159",
+  role: "guest",
+  assignRoom: "assignRoom1"
+})
 
 
 // weather
 
 app.post('/weather', (req, res) => {
-    const { city } = req.body;
+  const {
+    city
+  } = req.body;
 
-    console.log(city)
+  console.log(city)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84dda819f36a2f81e3babdb748579c85`)
-        .then(r => r.json())
-        .then(weather => {
-            res.send({ ok: true, weather })
-        })
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84dda819f36a2f81e3babdb748579c85`)
+    .then(r => r.json())
+    .then(weather => {
+      res.send({
+        ok: true,
+        weather
+      })
+    })
 })
 
 // user validation
-let users = [{userName:"ori",password:"1111"}]
+let users = [{
+  userName: "ori",
+  password: "1111"
+}]
 let saveduserName;
 
 app.post("/login", (req, res) => {
-  let { userName } = req.body;
-  let { password } = req.body;
+  let {
+    userName
+  } = req.body;
+  let {
+    password
+  } = req.body;
   let validation = false;
 
   users.forEach((e) => {
-    if (userName == e.userName && password == e.password) {
-      validation = true;
-      saveduserName = userName;
-    } else {
-      console.log(`Sorry ${e.userName} doesn't exist`);
-    }
+    if (user.find({
+        name: userName,
+        password: password
+
+      }, function (err, docs) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("First function call : ", docs);
+          console.log(docs[0].name);
+          if (docs[0].name == userName && docs[0].password == password) {
+            validation = true
+          } else {
+            validation = false
+            console.log(`Sorry ${e.userName} doesn't exist`);
+          }
+          console.log(validation)
+        }
+      })) {}
   });
 
   if (validation) {
-    res.cookie("User validated", userName, { maxAge: 30000, httpOnly: true });
-    
+    res.cookie("User validated", userName, {
+      maxAge: 30000,
+      httpOnly: true
+    });
+
   }
 
-  res.send({ validation });
+  res.send({
+    validation
+  });
 });
 
 // app.get("/get-userName", (req, res) => {
@@ -87,5 +141,5 @@ app.post("/login", (req, res) => {
 
 const PORT = 3030;
 app.listen(PORT, () => {
-    console.log(`RUNNING: ${PORT}`)
+  console.log(`RUNNING: ${PORT}`)
 })
