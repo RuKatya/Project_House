e58a7e6f4883c706bc840c36d5d9c73bfd5a1841
shneur <770abhtur1@gmail.com>
2021-01-25 20:36:05 +0200

    xdcvbn

--- house/server.js
+++ house/server.js
@@ -1,17 +1,25 @@
 const express = require('express')
-const app = express(); ///server;
-const bodyParser = require('body-parser');
+const jwt = require("jwt-simple");
 const fetch = require('node-fetch');
+const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
+const {
+    check
+} = require('express-validator')
+const {
+    validationResult
+} = require('express-validator')
+
+const app = express(); ///server;
+
 app.use(cookieParser());
 app.use(express.static('public'));
-app.use(bodyParser.json());
-const bcrypt = require('bcryptjs');
-const jwt = require("jwt-simple");
+app.use(express.json());
+
+
 const saltRounds = 7;
 const jwtSecret = "23";
-const {check} = require('express-validator')
-const {validationResult} = require('express-validator')
+
 
 //-----------mongoose----------//
 const mongoose = require('mongoose'); //npm i mongoose
@@ -20,75 +28,44 @@ const url = 'mongodb+srv://KatyaRu:qHO9SxoCGZc6lv7C@cluster0.mfqlq.mongodb.net/t
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}); //connectin to the db
+}).then(() => console.log("DB CONNECTION SUCCESSFUL")); //connectin to the db
+//connectin to the db
 
+//-----MODELS------//
 const User = mongoose.model('User', { //collection
     name: {
-type: String,
-unique: true,
-reqiired: true,
-trim: true
-    } , //with big letter !!!
- email: {
-     type: String,
-     unique: true,
-     required: true,
-     trim: true,
-     },    
-password: {
-    type: String,
-    required: true,
-    trim: true
-},
-role: {
-    type: String,
-    default: "user"
-},
-assignRooms:{
-    type: [String]
-}
-    //assignRooms: [String]
+        type: String,
+        unique: true,
+        required: [true, "To register you must enter username"],
+        trim: true
+    }, //with big letter !!!
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "To register you must enter Email"],
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "To register you must enter password"],
+        trim: true
+    },
+    role: {
+        type: String,
+        default: "child"
+    },
+    assignRooms: {
+        type: [String]
+    }
 });
 
-const Room = mongoose.model('Room', { //collection
-    roomName: String,
-    assignUser: [String],
+const Room = mongoose.model("Room", {
+    //collection
+    roomName: {
+        type: String,
+        required: true,
+    },
 });
-
-//-----------users-------------//
-const Shneor = new User({
-    name: "Shneor",
-    password: "123",
-    role: "admin",
-    assignRooms: ["assignRoom1"]
-})
-
-
-const Dudi = new User({
-    name: "Dudi",
-    password: "456",
-    role: "child",
-    assignRooms: ["assignRoom1", "assignRoom2"]
-})
-
-
-const Lior = new User({
-    name: "Lior",
-    password: "789",
-    role: "child",
-    assignRooms: ["assignRoom1"]
-})
-// Lior.save().then(doc => console.log(doc)).catch(e => {
-//     console.log(e)
-// })
-
-const Katya = new User({
-    name: "Katya",
-    password: "159",
-    role: "guest",
-    assignRooms: ["assignRoom1"]
-})
-
 
 
 // ---------ADMIN-----------//
@@ -97,7 +74,7 @@ isAdmin = (req, res, next) => {
     res.authorized = false;
     const {
         role
-    } = req.cookies
+    } = req.cookies;
 
 
     if (role === 'admin') {
@@ -108,134 +85,197 @@ isAdmin = (req, res, next) => {
     next()
 }
 
+// Client Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/rooms", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/rooms.html"));
+});
+
+//----------CHECK VALIDATION-------------//
+app.get("/api/read", (req, res) => {
+    let {
+        cookies
+    } = req;
+    if (cookies == admin || cookies == child)
+        res.send({
+            ok: true,
+        });
+});
+
+// Get all users
+app.get("/api/users", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).send({
+            users
+        });
+    } catch (err) {
+        res.status(404).send({
+            err
+        });
+    }
+});
+
+// Get user by id
+app.get("/api/users/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).send({
+            user
+        });
+    } catch (err) {
+        res.status(404).send({
+            err
+        });
+    }
+});
+
+// delete user by id
+app.delete("/api/users/:id", async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete(req.params.id);
+        res.status(200).send({
+            user
+        });
+    } catch (err) {
+        res.status(404).send({
+            err
+        });
+    }
+});
+
+// update user by id
+app.patch("/api/users/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        res.status(201).send({
+            user
+        });
+    } catch (err) {
+        res.status(400).send({
+            err
+        });
+    }
+});
+
 //----------LOGIN-------------//
-app.post("/login", async (req, res) => {
-    try{
-        const {name,password} = req.body;
-        /* let validation = false;
-        let role = 'denied'; */
-        const user = await User.findOne({name});
-        if(!user) {
-            return res.status(400).json({message: 'User is not found'})
+
+app.post("/api/login", async (req, res) => {
+    try {
+        const {
+            username,
+            password
+        } = req.body;
+     
+        const user = await User.findOne({
+            username
+        });
+        if (!user) {
+            return res.status(400).json({
+                message: 'User is not found'
+            })
         }
         const validPassword = bcrypt.compareSync(password, user.password)
-        if(!validPassword){
-            return res.status(400).json({message: 'invalid password'})
+        if (!validPassword) {
+            return res.status(400).json({
+                message: 'invalid password'
+            })
         }
-        const token = jwt.encode(
-            {
-              role: User.role,
-              name: User.name,
-              assignRooms: User. assignRooms,
-              time: new Date().getTime(),
-              id: User._id
+        const token = jwt.encode({
+                role: User.role,
+                username: User.username,
+                assignRooms: User.assignRooms,
+                time: new Date().getTime(),
+                id: User._id
             },
             jwtSecret
-          );
-          console.log(token)
-          res.cookie("token", token, {
+        );
+        console.log(token)
+        res.cookie("token", token, {
             maxAge: 1500000,
             httpOnly: true,
-          });
-          return res.json({status: 'allowed'})
-          
-    } catch (e){
+        });
+        return res.json({
+            status: 'allowed'
+        })
+
+    } catch (e) {
         console.log(e)
-        res.status(400).json({message:'Login error'})
+        res.status(400).json({
+            message: 'Login error'
+        })
     }
-     /* if (await doc.name == name && doc.password == password) {
-       
-        validation = true;
-        role = doc.role
-        res.cookie('role', role, {maxAge: 20000000,httpOnly: false});
-    } */
-  
-    //     res.cookie("User validated", name, { maxAge: 30000, httpOnly: true })
-    // } else {
-    //     alert(`Sorry ${e.name} doesn't exist`);
-    // }
-    // res.send({ validation });
+
 });
-//----------CHECK VALIDATION-------------//
-app.get('/read', (req, res) => {
-    let {cookies} = req
-    if (cookies == admin ||  cookies == child )
-    res.send({
-        ok: true
-    })
-})
 
-// let role = 'denied';
 
-// const indexUser = users.findIndex(User => user.name === name && user.password === password);
-// if (indexUser > -1) {
-//     isUserExist = true;
-//     role = users[indexUser].role;
-// }
-
-// let ok = false;
-// res.cookie('role', role, { maxAge: 20000000, httpOnly: false });
-
-// if (role === 'public' || role === 'admin') ok = true;
-
-// res.send({ ok })
 
 //-------------CREATE ACCOUNT-----------//
-app.post('/createAccount', [
-    check('name', 'Username cannot be empty').notEmpty(),check('email', 'username must be an email').isEmail(),
-check('password', 'password must be at least 3 - 10 characters').isLength({min:3, max:10})
+app.post("/api/register", [
+    check('name', 'Username cannot be empty').notEmpty(), check('email', 'username must be an email').isEmail(),
+    check('password', 'password must be at least 3 - 10 characters').isLength({
+        min: 3,
+        max: 10
+    })
 ], async (req, res) => {
     const errors = validationResult(req)
     console.log(errors)
     if (!errors.isEmpty()) {
-     return res.status(400).json({message: 'Registration error', errors})
-     
+        return res.status(400).json({
+            message: 'Registration error',
+            errors
+        })
+
     }
     console.log(errors)
-    const { name, email, password, checkPassword } = req.body;
-    console.log(req.body)
-  
-     const newUser = await new User({
-        name,
-        email,
-        password,
-        checkPassword
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).send({
+      newUser
     });
-    console.log(newUser)
 
 
-        bcrypt.hash(newUser.password, saltRounds, async function(err, hash) {
-            try {
-                console.log('hash:', hash)
-              newUser.password = hash;
-              await newUser.save();
-              console.log(newUser._id)
-              const token = jwt.encode(
-                {
-                  role: newUser.role,
-                  name: newUser.name,
-                  assignRooms: newUser. assignRooms,
-                  time: new Date().getTime(),
-                  id: newUser._id
+    bcrypt.hash(newUser.password, saltRounds, async  (err, hash) => {
+        try {
+            console.log('hash:', hash)
+            newUser.password = hash;
+            await newUser.save();
+            console.log(newUser._id)
+            const token = jwt.encode({
+                    role: newUser.role,
+                    name: newUser.name,
+                    assignRooms: newUser.assignRooms,
+                    time: new Date().getTime(),
+                    id: newUser._id
                 },
                 jwtSecret
-              );
-              console.log(token)
-              res.cookie("token", token, {
+            );
+            console.log(token)
+            res.cookie("token", token, {
                 maxAge: 1500000,
                 httpOnly: true,
-              });
-              res.send({ status: 'user registered successfully' });
-            } catch (e) {
-              console.log(e.message);
-              console.log(e.stack);
-              res.send({ status: 'Registration error' });
-              res.end();
-            }
-        });
-    
-})
+            });
+            res.send({
+                status: 'user registered successfully'
+            });
+        } catch (e) {
+            console.log(e.message);
+            console.log(e.stack);
+            res.send({
+                status: 'Registration error'
+            });
+            res.end();
+        }
+    });
 
+})
 
 //----------ROOM FUNCTIONS-------------//
 
@@ -268,17 +308,22 @@ app.delete('/deleteroom', async (req, res) => {
 
 //-------------UPDATE ROOM--------------//
 app.put('/updateRoom', async (req, res) => {
-    
+
     const {
         assignUser,
         roomName,
         Selectedroom
     } = req.body
-    let lastRoomId = await Room.find({roomName:Selectedroom})
+    let lastRoomId = await Room.find({
+        roomName: Selectedroom
+    })
     console.log("lastRoomId: ", lastRoomId)
     let roomID = lastRoomId._id
     console.log("roomID: ", roomID)
-    await Room.findByIdAndUpdate(roomID,{roomName:roomName, assignUser: assignUser})
+    await Room.findByIdAndUpdate(roomID, {
+        roomName: roomName,
+        assignUser: assignUser
+    })
     const data = await Room.find({})
     console.log("data: ", data)
     res.send({
