0ccbd7a0215b72b3592f28e265d51193e5282722
shneurGreenberg <72753321+shneurGreenberg@users.noreply.github.com>
2021-01-31 19:40:44 +0200

    Add files via upload

---
+++ house/Public/roomsJS.js
@@ -0,0 +1,193 @@
+  //---------CORRECT TIME-------//
+  let date = new Date();
+  getTime = () => {
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+      if (seconds < 10) {
+          seconds = '0' + seconds;
+      }
+
+      if (minutes < 10) {
+          minutes = '0' + minutes;
+      }
+
+      let time = `${hours}:${minutes}:${seconds}`;
+      document.getElementById('correctTime').innerHTML = time;
+  }
+  setInterval(getTime, 0);
+
+  //---------CORRECT DATE---------//
+  getDate = () => {
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear()
+
+      if (day < 10) {
+          day = '0' + day;
+      }
+      if (month < 10) {
+          month = '0' + month;
+      }
+
+      let dateToday = `${day}/${month}/${year}`
+      document.getElementById('correctDate').innerHTML = dateToday
+
+  }
+  setInterval(getDate, 0)
+
+  //-----RENDER ROOMS PAGE-------// 
+
+  getAllRooms = (rooms) => {
+      
+      let display = ''
+      fetch("/api/allrooms")
+          .then(r => r.json())
+          .then(data => {
+              data.rooms.forEach(room => {
+                  const listTasks = room.notes.map((task) => `<div>${task}
+                      <button id="${room._id}" name="${task}" class="deleteTask"
+                          onclick="handleDeleteTask(event)">Done</button>
+                  </div>`).join(' ')
+
+                  display += `<div class="roomsAndTask" onclick="onClickRoom(event)">
+                      <div class="gridHeadline">
+                          <h3>${room.roomName}</h3>
+                          <button id="${room._id}" onclick="handleDeleteRoom(event)" class="deleteRoom">Delete
+                              room</button>
+                      </div>
+                      <form id="${room._id}" class="formTask" onsubmit='handleAddTask(event)'>
+                          <input class="newTask" type='text' placeholder="add task" name='newTask' required>
+                          <input type="submit" class="addTask" value="Add task">
+                      </form>
+                      <div class="listTask">${listTasks}</div>
+                  </div>`
+              })
+              document.getElementById('putRoom').innerHTML = display
+          })
+
+  }
+  // // SHOW SINGEL ROOM
+
+  onClickRoom = (e) => {
+      let showRoom = ''
+      console.log("room clickd")
+
+      fetch("/api/allrooms")
+          .then(r => r.json())
+          .then(data => {
+              data.rooms.forEach(room => {
+                  const listTasks = room.notes.map((task) => `<div>${task}
+                      <button id="${room._id}" name="${task}" class="deleteTask"
+                          onclick="handleDeleteTask(event)">Done</button>
+                  </div>`).join(' onClickRoom')
+
+                  showRoom += `<div class="body">
+                      <div class="back">
+                          <a href="/rooms.html"><img src="icons/back.svg"></a>
+                      </div>
+
+                      <div>
+                          <h1>${room.roomName}</h1>
+                      </div>
+                      <div class="notes">
+                          ${listTasks}
+                      </div>
+                  </div>`
+              })
+              document.getElementById('singelRoom').innerHTML = showRoom
+          })
+
+  }
+
+
+  //-----DELETE ROOM-------//
+  handleDeleteRoom = (e) => {
+      e.preventDefault()
+      const roomId = e.target.id
+      fetch('/api/deleteroom', {
+              method: 'delete',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  roomId
+              })
+          }).then(r => r.json())
+          .then(data => {
+              console.log(data)
+              getAllRooms()
+          })
+  }
+
+  //-----CREATE ROOM-------//
+  hendleCreateRoom = (e) => {
+      e.preventDefault()
+      const roomName = e.target.children.roomName.value
+      const room = document.getElementById('putRoom')
+      fetch('/api/room', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  roomName
+              })
+          }).then(r => r.json())
+          .then(data => {
+
+              getAllRooms()
+          })
+  }
+
+  //-----CREATE TASK-------//
+  handleAddTask = (e) => {
+      e.preventDefault()
+      const createTask = e.target.children.newTask.value
+      const roomId = e.target.id
+
+      fetch('/api/notes', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  createTask,
+                  roomId
+              })
+          }).then(r => r.json())
+          .then(data => {
+              console.log(data)
+              getAllRooms()
+          })
+  }
+
+  //-----DELETE TASK-------//
+  handleDeleteTask = (e) => {
+      e.preventDefault()
+      const roomId = e.target.id
+      const deleteTask = e.target.name
+
+      fetch('/api/deletenotes', {
+              method: 'delete',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  deleteTask,
+                  roomId
+              })
+          }).then(r => r.json())
+          .then(data => {
+              console.log(data)
+              getAllRooms()
+          })
+  }
+
+  document.addEventListener('DOMContentLoaded', getAllRooms());
+  let eeee = 1
+  if(1 ==1){
+
+    console.log("rtyuibvfyuj")
+  }
+  let eeee = 12
